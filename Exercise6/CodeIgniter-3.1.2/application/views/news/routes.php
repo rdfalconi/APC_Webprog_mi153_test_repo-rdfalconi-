fa4b386d50aa29dcd2c3d7ae90c8eecf8b40a563
richard miguel d. falconi <rdfalconi@student.apc.edu.ph>
2016-12-22 14:37:45 +0800

    routes.php

---
+++ Exercise6/CodeIgniter-3.1.2/application/views/news/routes.php
@@ -0,0 +1,7 @@
+c$route['news'] = 'news';
+$route['news/create'] = 'news/create';
+ 
+$route['news/edit/(:any)'] = 'news/edit/$1';
+ 
+$route['news/view/(:any)'] = 'news/view/$1';
+$route['news/(:any)'] = 'news/view/$1';
