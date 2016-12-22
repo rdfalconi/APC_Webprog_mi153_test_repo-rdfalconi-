03bb99ede79c4e46c133aca9a277d6f88ecc0c4c
richard miguel d. falconi <rdfalconi@student.apc.edu.ph>
2016-12-22 15:04:42 +0800

    delete some files

--- Exercise6/CodeIgniter-3.1.2/application/views/news/routes.php
+++
@@ -1,7 +0,0 @@
-c$route['news'] = 'news';
-$route['news/create'] = 'news/create';
- 
-$route['news/edit/(:any)'] = 'news/edit/$1';
- 
-$route['news/view/(:any)'] = 'news/view/$1';
-$route['news/(:any)'] = 'news/view/$1';
